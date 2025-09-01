ae47880de42502be2437066eab28dcdc36646380
Reeshy <64738767+reeshy@users.noreply.github.com>
2025-09-01 23:35:15 +0200

    App Created

---
+++ KeyboardGlowShortcut/KeyboardGlowShortcut/KeyboardGlowShortcutApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardGlowShortcutApp.swift
+//  KeyboardGlowShortcut
+//
+//  Created by Rashad Rafa on 2025-09-01.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardGlowShortcutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeyboardGlowShortcut/KeyboardGlowShortcutTests/KeyboardGlowShortcutTests.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardGlowShortcutTests.swift
+//  KeyboardGlowShortcutTests
+//
+//  Created by Rashad Rafa on 2025-09-01.
+//
+
+import Testing
+@testable import KeyboardGlowShortcut
+
+struct KeyboardGlowShortcutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
